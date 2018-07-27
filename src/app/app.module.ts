0c06c86463e8f954da937f54d7d063d0ed792c58
vic lotana <viclotana@gmail.com>
2018-07-27 21:38:07 +0100

    import angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 
 
