7b3dd66e164194d7ba18bf0cafbd31555d77481a
vic lotana <viclotana@gmail.com>
2018-07-27 21:39:47 +0100

    import firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
 
 
