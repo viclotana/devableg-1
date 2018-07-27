7910908be0b5fac0e7b2ce69467a2d894d808daf
vic lotana <viclotana@gmail.com>
2018-07-27 21:36:47 +0100

    import environment in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
