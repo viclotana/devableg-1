34d5d12e04b4edeee5b4c2ade5c6146731adf700
vic lotana <viclotana@gmail.com>
2018-07-27 21:35:59 +0100

    adding keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    
+    apiKey: "AIzaSyCQ5yXXjrDOp1sYlmR4TM1wMUW7le4P780",
+    authDomain: "nollywood1-358bf.firebaseapp.com",
+    databaseURL: "https://nollywood1-358bf.firebaseio.com",
+    projectId: "nollywood1-358bf",
+    storageBucket: "nollywood1-358bf.appspot.com",
+    messagingSenderId: "1036356969034"
   }
 };
 
