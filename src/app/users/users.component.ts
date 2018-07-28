88ac5c917b9010f44ce5ce7a9b8edad13183d388
vic lotana <viclotana@gmail.com>
2018-07-28 21:48:13 +0100

    linting

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -27,7 +27,7 @@ import { Dev } from '../models/dev';
 })
 export class UsersComponent implements OnInit {
   devs: Dev[];
-  
+
 
   constructor(private devService: DevService, private data: DataService) { }
 
