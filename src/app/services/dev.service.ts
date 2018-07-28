34575fa4e79a6486bfc707cd53a4860652fc4ab5
vic lotana <viclotana@gmail.com>
2018-07-28 19:56:06 +0100

    auto imported observable

--- src/app/services/dev.service.ts
+++ src/app/services/dev.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from
 'angularfire2/firestore';
 import { Dev } from '../models/dev';
+import { Observable } from '../../../node_modules/rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class DevService {
    devsCollection: AngularFirestoreCollection<Dev>;
-
+   devs: Observable<Dev[]>;
   constructor(public lax: AngularFirestore) { }
 }
 
