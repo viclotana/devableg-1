f2cb1daa94dbfaf5e748a6fac4f2516f5bf24659
vic lotana <viclotana@gmail.com>
2018-07-28 19:50:03 +0100

    updates5

--- src/app/services/dev.service.ts
+++ src/app/services/dev.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from
 'angularfire2/firestore';
+import { Dev } from '../models/dev';
 @Injectable({
   providedIn: 'root'
 })
