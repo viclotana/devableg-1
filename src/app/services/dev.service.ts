e090c9063f86074cbd8ac60717cac6f9d102c0ef
vic lotana <viclotana@gmail.com>
2018-07-28 19:39:28 +0100

    injecting firebase in the service

--- src/app/services/dev.service.ts
+++ src/app/services/dev.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from
+'angularfire2/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class DevService {
+   devsCollection: AngularFirestoreCollection<Dev>
 
-  constructor() { }
+  constructor(public lax: AngularFirestore) { }
 }
+
