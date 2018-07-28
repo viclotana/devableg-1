ec4665b026c521b13324e82e24b1760b3470f4a5
vic lotana <viclotana@gmail.com>
2018-07-28 21:44:38 +0100

    updates end

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -10,20 +10,20 @@ from '@angular/animations'
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
-  animations: [
-    trigger('listStagger', [
-      transition('* <=> *', [
-        query(':enter', 
-        [
-          style({ opacity: 0, transform: 'translateY(-35px)' }),
-          stagger('50ms', 
-          animate('550ms ease-out',
-        style({ opacity: 1, transform: 'translateY(0px)'})))
-        ], {optional: true}),
-        query(':leave', animate('50ms', style({ opacity: 0 })),{optional: true})
-      ])
-    ]) 
-  ]
+  // animations: [
+  //   trigger('listStagger', [
+  //     transition('* <=> *', [
+  //       query(':enter', 
+  //       [
+  //         style({ opacity: 0, transform: 'translateY(-35px)' }),
+  //         stagger('50ms', 
+  //         animate('550ms ease-out',
+  //       style({ opacity: 1, transform: 'translateY(0px)'})))
+  //       ], {optional: true}),
+  //       query(':leave', animate('50ms', style({ opacity: 0 })),{optional: true})
+  //     ])
+  //   ]) 
+  // ]
 })
 export class UsersComponent implements OnInit {
   devs: Dev[];
